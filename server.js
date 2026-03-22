47e5e9163886074cce2633759e7ad2b1f481b97d
T1me1k <76596032+T1me1k@users.noreply.github.com>
2026-03-22 04:54:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const launcherConfig = {
   appName: "TRUST",
   minSupportedVersion: "0.1.0",
   latestVersion: "0.1.0",
-  matchmakingEnabled: true,
+  matchmakingEnabled: false,
   maintenance: false,
   motd: "Welcome to TRUST alpha",
   news: [
